4999e420af6201b499a2c1b24a0ff3bd4faa89f4
cer0311 <cernohorskylukas@gmail.com>
2016-10-05 10:20:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html dir="ltr" lang="">
+  <head>
+  
+    <title>PAW</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	
+	
+</head>
+<body>
+	<h1>Tohle je nadpis mých webových stránek v php pro PAW</h1>
+	<a href="https://github.com/cer0311">Navštivte muj GITHUB, kde najdete tento velmi, velmi, opakuju velmi zajímavý kód</a>
+	<h2><img src="https://cdn.meme.am/instances/500x/18399212.jpg" alt="Our boss Rogers, what a cool guy" style="width:407px;height:405px;"></h2>
+<?php
+	echo "<h3>Ahoj Rogersi čus</h3>";
+	for ($i=0; $i<10; $i++) {
+		echo $i.  "<h4>Nevím</h4>";
+		
+	}
+?>
+
+</body>
+</html>
